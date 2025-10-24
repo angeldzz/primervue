0babe2751301cf95594ea0f8c685cb57e2bd9c90
angeldzz <pintodiazangel@gmail.com>
2025-10-24 10:13:05 +0200

    primeros cambios VUE variables dentro de data()

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HolaMundo msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HolaMundo from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HolaMundo
   }
 }
 </script>
